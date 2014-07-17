a5d6179d1bc2fddaae76095da346d978f90b4719
Jeong Won Cheol <sturdyegg@gmail.com>
2014-07-17 12:11:33 +0900

    ladder_01

--- 04.Ladder/src/Ladder.java
+++ 04.Ladder/src/Ladder.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Marker {
 	private int x;
 	private int y;
@@ -43,7 +45,7 @@ class Mover {
 	}
 
 	int checkLadderLinePos(int[][] ladder) {
-		if (marker.getYPosition() == 0) // 왼쪽 라인일 때
+		if (marker.getYPosition() == 0) 									// 왼쪽 라인일 때
 		{
 			return 1;
 		} else if (marker.getYPosition() == ladder[0].length - 1) // 오른쪽 라인일 때
@@ -88,16 +90,51 @@ class Mover {
 public class Ladder {
 
 	int[][] ladder;
-
+	int line;
+	boolean exitSw;
+	
+	String str;
+	Scanner sc;
+	
 	Ladder() {
 		ladder = new int[4][3];
 		ladder[0][0] = 1;
 		ladder[1][1] = 1;
 		ladder[2][0] = 1;
 		ladder[3][1] = 1;
+		
+		exitSw = false;
 	}
 
-	void rideLadder(int line) {
+	void lineInput() {
+		System.out.print(">");
+		sc = new Scanner(System.in);
+		str = sc.next();
+		if(tryConvertToInt(str) && checkBound())
+			rideLadder();
+	}
+	
+	boolean tryConvertToInt(String str){
+		try{
+			line = Integer.parseInt(str);
+			return true;
+		}catch(NumberFormatException e){
+			System.out.println(e.getMessage());
+			System.out.println("프로그램을 종료합니다.");
+			exit();
+			return false;
+		}
+	}
+	
+	boolean checkBound()
+	{
+		if(line > 0 && line <= ladder[0].length)
+			return true;
+		System.out.println("사다리 입력 범위를 초과하였습니다. [입력범위 : 0 - " + ladder[0].length + "]");
+		return false;
+	}
+
+	void rideLadder() {
 		Mover mover = new Mover(line);
 
 		for (int i = 0; i < ladder.length; i++) {
@@ -107,12 +144,29 @@ public class Ladder {
 		int result = mover.marker.getYPosition() + 1;
 		System.out.println(result);
 	}
+	
+	void printLadder(){
+		
+	}
+	
+	void exit()
+	{
+		exitSw = true;
+		sc.close();
+	}
+	
+	boolean isExit()
+	{
+		return exitSw;
+	}
 
 	public static void main(String[] args) {
 
 		Ladder ladder = new Ladder();
-		ladder.rideLadder(1);
-
+		while(!ladder.isExit())
+		{
+			ladder.lineInput();
+		}
 	}
 
 }
